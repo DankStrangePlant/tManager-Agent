f48240631ff63a4b6830933b630d09cdfa4a1549
Prismism0 <59492869+Prismism0@users.noreply.github.com>
2020-03-04 19:32:40 -0500

    Finally got a unit test project in
    And it doesn't break the TML build step!

---
+++ TManagerAgentTest/Core/JsonTest.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using TManagerAgent.Core;
+
+namespace TManagerAgentTest.Core
+{
+    [TestFixture]
+    class JsonTest
+    {
+        private JsonSerializerSettings JsonSerializerSettings;
+
+        private MemberRoute BasicRoute1 = new MemberRoute(MemberContext.StaticMainMemberContext, "myProperty");
+        private MemberRoute BasicRoute2 = new MemberRoute(MemberContext.StaticMainMemberContext, "myField");
+
+        [OneTimeSetUp]
+        public void Begin()
+        {
+            JsonSerializerSettings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            };
+            JsonConvert.DefaultSettings = () => JsonSerializerSettings;
+        }
+
+
+        #region Helper Functions
+        private static string SerializeAndPrint(object toSerialize)
+        {
+            string serialized = JsonConvert.SerializeObject(toSerialize);
+
+            TestContext.WriteLine($"TYPE {toSerialize.GetType()} SERIALIZED TO:");
+            TestContext.WriteLine(serialized);
+
+            return serialized;
+        }
+        #endregion
+
+        #region MemberRoute
+        [Test]
+        public void Test_MemberRoute_Serialize()
+        {
+            MemberRoute route = BasicRoute1;
+
+            var output = SerializeAndPrint(route);
+            Assert.That(output != null && output.Length > 0);
+        }
+
+        [Test]
+        public void Test_MemberRoute_Deserialize()
+        {
+            string input = @"{
+  'Context': {
+    'ContextType': 'Main',
+    'ContextKey': null
+  },
+  'AccessorKey': 'raining',
+  'Instance': false
+}";
+
+            MemberRoute route = JsonConvert.DeserializeObject<MemberRoute>(input);
+
+            Assert.That(route.Context.ContextType == MemberContextType.Main);
+            Assert.That(route.Context.ResolvedContext == null);
+            Assert.That(route.Context.Static);
+            Assert.That(route.AccessorKey == "raining");
+            Assert.That(!route.Instance);
+        }
+
+        [Test]
+        public void Test_MemberRoute_Full()
+        {
+            MemberRoute route = new MemberRoute(MemberContext.StaticMainMemberContext, "raining");
+            var sameRoute = JsonConvert.DeserializeObject<MemberRoute>(JsonConvert.SerializeObject(route));
+
+            Assert.That(sameRoute, Is.EqualTo(route));
+        }
+        #endregion
+
+
+    }
+}
