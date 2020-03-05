2998a9f9aefd3817e5907a731110c5d8bd0b09d8
Prismism0 <59492869+Prismism0@users.noreply.github.com>
2020-03-04 21:31:27 -0500

    Adding ModConfig
    With separate implementation (TODO at a later date) for server

---
+++ Config.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel;
+using static Terraria.ModLoader.ModContent;
+
+namespace TManagerAgent
+{
+    public interface IAgentConfig
+    {
+        string OverseerAddress { get; set; }
+
+        int OverseerPort { get; set; }
+    }
+
+    public class RawAgentConfig : IAgentConfig
+    {
+        #region Constructors
+        public RawAgentConfig(string address, ushort port)
+        {
+            OverseerAddress = address;
+            OverseerPort = port;
+        }
+        #endregion
+
+        #region IAgentConfig
+        public string OverseerAddress { get; set; } = TManagerAgent.DEFAULT_OVERSEER_ADDRESS;
+
+        [Range(ushort.MinValue, ushort.MaxValue)]
+        public int OverseerPort { get; set; } = TManagerAgent.DEFAULT_OVERSEER_PORT;
+        #endregion
+
+        #region Static Util
+        public static RawAgentConfig Load()
+        {
+            // TODO ... actually load something.......
+            return new RawAgentConfig(TManagerAgent.DEFAULT_OVERSEER_ADDRESS, TManagerAgent.DEFAULT_OVERSEER_PORT);
+        }
+        #endregion
+    }
+
+    public class ClientAgentConfig : ModConfig, IAgentConfig
+    {
+        #region Override
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        public override void OnChanged()
+        {
+            base.OnChanged();
+
+            GetInstance<TManagerAgent>().LoadConfig(false);
+        }
+        #endregion
+
+        #region IAgentConfig
+        [Label("Overseer Address")]
+        [DefaultValue(TManagerAgent.DEFAULT_OVERSEER_ADDRESS)]
+        public string OverseerAddress { get; set; }
+        [Label("Overseer Port")]
+        [Range(ushort.MinValue, ushort.MaxValue)]
+        [DefaultValue(TManagerAgent.DEFAULT_OVERSEER_PORT)]
+        public int OverseerPort { get; set; }
+        #endregion
+    }
+}
--- TManagerAgent.cs
+++ TManagerAgent.cs
@@ -1,13 +1,37 @@
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
 
 namespace TManagerAgent
 {
     public class TManagerAgent : Mod
     {
+        #region Const
+        public const string LOCALHOST = "127.0.0.1";
+
+        public const string DEFAULT_OVERSEER_ADDRESS = LOCALHOST;
+        public const ushort DEFAULT_OVERSEER_PORT = 3006;
+
+        public const ushort DEFAULT_INCOMING_PORT = 3005;
+        #endregion
+
+        #region Config
+        public string OverseerAddress { get; private set; }
+        public ushort OverseerPort { get; private set; }
+        #endregion
+
         public TManagerAgent()
         {
-            
+        }
 
+        #region Override
+        public override void Load()
+        {
+            base.Load();
+
+            LoadConfig(Main.netMode == NetmodeID.Server);
+            
         }
 
         public override void PreSaveAndQuit()
@@ -17,5 +41,23 @@ namespace TManagerAgent
             TManagerAgentWorld world = ModContent.GetInstance<TManagerAgentWorld>();
             world.PreCloseWorld();
         }
+        #endregion
+
+        #region Private
+        public void LoadConfig(bool server)
+        {
+            IAgentConfig config = server ?
+                (IAgentConfig)RawAgentConfig.Load() :
+                (IAgentConfig)GetInstance<ClientAgentConfig>();
+
+            OverseerAddress = config?.OverseerAddress ?? DEFAULT_OVERSEER_ADDRESS;
+            OverseerPort = (ushort)(config?.OverseerPort ?? DEFAULT_OVERSEER_PORT);
+
+            if (config is null)
+            {
+                Logger.Info("Config not loaded! Using defaults.");
+            }
+        }
+        #endregion
     }
 }
